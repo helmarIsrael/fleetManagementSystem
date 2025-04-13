dd10d432c6d4be76b28d9d6eec3d519b6ed09eae
helmarIsrael <helmar.israel@g.msuiit.edu.ph>
2025-04-14 01:52:05 +0800

    ADd functions for dart

--- fleetmanagementsystem/lib/functions/functions.dart
+++ fleetmanagementsystem/lib/functions/functions.dart
@@ -3,24 +3,70 @@ import 'dart:convert';
 import 'package:fleetmanagementsystem/services/apiUrl.dart';
 
 Future<List> fetchWorkerLocation() async {
-    try {
-      final Uri url = Uri.http(apiUrl, '/worker_location');
+  try {
+    final Uri url = Uri.http(apiUrl, '/worker_location');
 
-      final response = await http.get(
-        url,
-        // headers: {
-        //   'Authorization': 'Bearer $accessToken',
-        // },
-      );
+    final response = await http.get(
+      url,
+      // headers: {
+      //   'Authorization': 'Bearer $accessToken',
+      // },
+    );
 
-      if (response.statusCode == 200) {
-        final dynamic responseData = json.decode(response.body);
-        return responseData;
-      } else {
-        throw Exception('Failed to load: ${response.statusCode}');
-      }
-    } catch (e) {
-      print(e);
-      return [];
+    if (response.statusCode == 200) {
+      final dynamic responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      throw Exception('Failed to load: ${response.statusCode}');
     }
+  } catch (e) {
+    return [];
   }
+}
+
+Future<List> fetchClientLocation() async {
+  try {
+    final Uri url = Uri.http(apiUrl, '/client_location');
+
+    final response = await http.get(
+      url,
+      // headers: {
+      //   'Authorization': 'Bearer $accessToken',
+      // },
+    );
+
+    if (response.statusCode == 200) {
+      final dynamic responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      throw Exception('Failed to load: ${response.statusCode}');
+    }
+  } catch (e) {
+    return [];
+  }
+}
+
+Future<List<dynamic>> fetchClientAndWorkerDistance(
+  Map<String, dynamic> data,
+) async {
+  try {
+    final Uri url = Uri.http(apiUrl, '/client_worker_distance');
+
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      final dynamic responseData = json.decode(response.body);
+      print("The response data is: ${responseData['data']}");
+      return responseData['data'];
+    } else {
+      throw Exception('Failed to load: ${response.statusCode}');
+    }
+  } catch (e) {
+    print(e);
+    return [{}];
+  }
+}
